0e3f79916e615a99677c658effa746321d9f55b0
Dustin Collins <55062873+STREGAsGate@users.noreply.github.com>
2021-03-23 18:21:33 -0400

    Implement D3DShaderBytecode.

--- Sources/Direct3D12/d3d12_h/Structures/D3D12_SHADER_BYTECODE.swift
+++ Sources/Direct3D12/d3d12_h/Structures/D3D12_SHADER_BYTECODE.swift
@@ -7,11 +7,41 @@
  */
 
 import WinSDK
+import Foundation
 
+/// Describes shader data.
 public struct D3DShaderBytecode {
     public typealias RawValue = WinSDK.D3D12_SHADER_BYTECODE
     internal var rawValue: RawValue
 
+    /// A pointer to a memory block that contains the shader data.
+    public var data: Data? {
+        get {
+            guard rawValue.pShaderBytecode != nil else {return nil}
+            return withUnsafeBytes(of: rawValue.pShaderBytecode) {
+                return Data($0)
+            }
+        }
+        set {
+            if let newValue = newValue {
+                newValue.withUnsafeBytes {
+                    self.rawValue.pShaderBytecode = $0.baseAddress!
+                }
+                rawValue.BytecodeLength = SIZE_T(newValue.count)
+            }else{
+                rawValue.pShaderBytecode = nil
+                rawValue.BytecodeLength = 0
+            }
+        }
+    }
+
+    /** Describes shader data.
+    - parameter data: A pointer to a memory block that contains the shader data.
+    */
+    public init(data: Data?) {
+        self.rawValue = RawValue()
+        self.data = data
+    }
 
     internal init(_ rawValue: RawValue) {
         self.rawValue = rawValue
