49ac59e521d029b71432c8e779df984680d08b7b
Dustin Collins <55062873+STREGAsGate@users.noreply.github.com>
2021-03-23 18:12:22 -0400

    fix doc comment

--- Sources/Direct3D12/d3d12_h/Structures/D3D12_SAMPLER_DESC.swift
+++ Sources/Direct3D12/d3d12_h/Structures/D3D12_SAMPLER_DESC.swift
@@ -113,7 +113,18 @@ public struct D3DSamplerDescription {
         }
     }
 
-    /// Describes a sampler state.
+    /** Describes a sampler state.
+    - parameter filter: A D3D12_FILTER-typed value that specifies the filtering method to use when sampling a texture.
+    - parameter addressU: A D3D12_TEXTURE_ADDRESS_MODE-typed value that specifies the method to use for resolving a u texture coordinate that is outside the 0 to 1 range.
+    - parameter addressV: A D3D12_TEXTURE_ADDRESS_MODE-typed value that specifies the method to use for resolving a v texture coordinate that is outside the 0 to 1 range.
+    - parameter addressW: A D3D12_TEXTURE_ADDRESS_MODE-typed value that specifies the method to use for resolving a w texture coordinate that is outside the 0 to 1 range.
+    - parameter mipLODBias: Offset from the calculated mipmap level. For example, if the runtime calculates that a texture should be sampled at mipmap level 3 and MipLODBias is 2, the texture will be sampled at mipmap level 5.
+    - parameter maxAnisotropy: Clamping value used if D3D12_FILTER_ANISOTROPIC or D3D12_FILTER_COMPARISON_ANISOTROPIC is specified in Filter. Valid values are between 1 and 16.
+    - parameter comparisonFunction: A D3D12_COMPARISON_FUNC-typed value that specifies a function that compares sampled data against existing sampled data.
+    - parameter borderColor: Border color to use if D3D12_TEXTURE_ADDRESS_MODE_BORDER is specified for AddressU, AddressV, or AddressW. Range must be between 0.0 and 1.0 inclusive.
+    - parameter minLOD: Lower end of the mipmap range to clamp access to, where 0 is the largest and most detailed mipmap level and any level higher than that is less detailed.
+    - parameter maxLOD: Upper end of the mipmap range to clamp access to, where 0 is the largest and most detailed mipmap level and any level higher than that is less detailed. This value must be greater than or equal to MinLOD. To have no upper limit on LOD, set this member to a large value.
+    */
     public init(filter: D3DFilter,
                 addressU: D3DTextureAddressMode,
                 addressV: D3DTextureAddressMode,
