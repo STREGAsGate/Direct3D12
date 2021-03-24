20a66ff7daa4d56bb606a356ed4dc9f8a6fff68a
Dustin Collins <55062873+STREGAsGate@users.noreply.github.com>
2021-03-24 18:20:44 -0400

    Implement D3DTexture3DRenderTargetView.

--- Sources/Direct3D12/d3d12_h/Structures/D3D12_TEX3D_RTV.swift
+++ Sources/Direct3D12/d3d12_h/Structures/D3D12_TEX3D_RTV.swift
@@ -8,9 +8,49 @@
 
 import WinSDK
 
+/// Describes the subresources from a 3D texture to use in a render-target view.
 public struct D3DTexture3DRenderTargetView {
     public typealias RawValue = WinSDK.D3D12_TEX3D_RTV
     internal var rawValue: RawValue
+
+    /// The index of the mipmap level to use.
+    public var mipIndex: UInt32 {
+        get {
+            return rawValue.MipSlice
+        }
+        set {
+            rawValue.MipSlice = newValue
+        }
+    }
+
+    /// First depth level to use.
+    public var depthIndex: UInt32 {
+        get {
+            return rawValue.FirstWSlice
+        }
+        set {
+            rawValue.FirstWSlice = newValue
+        }
+    }
+
+    /// Number of depth levels to use in the render-target view, starting from FirstWSlice. A value of -1 indicates all of the slices along the w axis, starting from FirstWSlice.
+    public var depthCount: UInt32 {
+        get {
+            return rawValue.WSize
+        }
+        set {
+            rawValue.WSize = newValue
+        }
+    }
+
+    /** Describes the subresources from a 3D texture to use in a render-target view.
+    - parameter mipIndex: The index of the mipmap level to use mip slice.
+    - parameter depthIndex: First depth level to use.
+    - parameter depthCount: Number of depth levels to use in the render-target view, starting from FirstWSlice. A value of -1 indicates all of the slices along the w axis, starting from FirstWSlice.
+    */
+    public init(mipIndex: UInt32, depthIndex: UInt32, depthCount: UInt32) {
+        self.rawValue = RawValue(MipSlice: mipIndex, FirstWSlice: depthIndex, WSize: depthCount)
+    }
 
     internal init(_ rawValue: RawValue) {
         self.rawValue = rawValue
