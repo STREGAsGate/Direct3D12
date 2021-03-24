b03669783fc2df94771b82d208ed43c52d4fa288
Dustin Collins <55062873+STREGAsGate@users.noreply.github.com>
2021-03-24 17:33:58 -0400

    Implement D3DSubresourceTiling.

--- Sources/Direct3D12/d3d12_h/Structures/D3D12_SUBRESOURCE_TILING.swift
+++ Sources/Direct3D12/d3d12_h/Structures/D3D12_SUBRESOURCE_TILING.swift
@@ -8,11 +8,68 @@
 
 import WinSDK
 
+/// Describes a tiled subresource volume.
 public struct D3DSubresourceTiling {
-    internal var rawValue: WinSDK.D3D12_SUBRESOURCE_TILING
+    public typealias RawValue = WinSDK.D3D12_SUBRESOURCE_TILING
+    internal var rawValue: RawValue
 
+    /// The width in tiles of the subresource.
+    public var horizontalTileCount: UInt32 {
+        get {
+            return rawValue.WidthInTiles
+        }
+        set {
+            rawValue.WidthInTiles = newValue
+        }
+    }
 
-    internal init(_ rawValue: WinSDK.D3D12_SUBRESOURCE_TILING) {
+    /// The height in tiles of the subresource.
+    public var verticalTileCount: UInt16 {
+        get {
+            return rawValue.HeightInTiles
+        }
+        set {
+            rawValue.HeightInTiles = newValue
+        }
+    }
+
+    /// The depth in tiles of the subresource.
+    public var depthTileCount: UInt16 {
+        get {
+            return rawValue.DepthInTiles
+        }
+        set {
+            rawValue.DepthInTiles = newValue
+        }
+    }
+
+    /// The index of the tile in the overall tiled subresource to start with.
+    public var startIndex: UInt32 {
+        get {
+            return rawValue.StartTileIndexInOverallResource
+        }
+        set {
+            rawValue.StartTileIndexInOverallResource = newValue
+        }
+    }
+
+    /** Describes a tiled subresource volume.
+    - parameter horizontalTileCount: The width in tiles of the subresource.
+    - parameter verticalTileCount: The height in tiles of the subresource.
+    - parameter depthTileCount: The depth in tiles of the subresource.
+    - parameter startIndex: The index of the tile in the overall tiled subresource to start with.
+    */
+    public init(horizontalTileCount: UInt32,
+                verticalTileCount: UInt16,
+                depthTileCount: UInt16,
+                startIndex: UInt32) {
+        self.rawValue = RawValue(WidthInTiles: horizontalTileCount, 
+                                 HeightInTiles: verticalTileCount, 
+                                 DepthInTiles: depthTileCount, 
+                                 StartTileIndexInOverallResource: startIndex)
+    }
+
+    internal init(_ rawValue: RawValue) {
         self.rawValue = rawValue
     }
 }
