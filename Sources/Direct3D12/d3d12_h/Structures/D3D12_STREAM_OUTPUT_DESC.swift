81c5551100d9b2117df2f9a3919dc1f5026d36f5
Dustin Collins <55062873+STREGAsGate@users.noreply.github.com>
2021-03-24 17:15:54 -0400

    Implement D3DStreamOutputDescription.

--- Sources/Direct3D12/d3d12_h/Structures/D3D12_STREAM_OUTPUT_DESC.swift
+++ Sources/Direct3D12/d3d12_h/Structures/D3D12_STREAM_OUTPUT_DESC.swift
@@ -8,11 +8,60 @@
 
 import WinSDK
 
+/// Describes a streaming output buffer.
 public struct D3DStreamOutputDescription {
     public typealias RawValue =  WinSDK.D3D12_STREAM_OUTPUT_DESC
     internal var rawValue: RawValue
 
-    
+    /// An array of D3D12_SO_DECLARATION_ENTRY structures. Can't be NULL if NumEntries > 0.
+    public var declarationEntries: [D3DStreamOutputDeclarationEntry] {
+        get {
+            let buffer = UnsafeBufferPointer(start: rawValue.pSODeclaration, count: Int(rawValue.NumEntries))
+            return buffer.map({D3DStreamOutputDeclarationEntry($0)})
+        }
+        set {
+            newValue.map({$0.rawValue}).withUnsafeBufferPointer {
+                rawValue.pSODeclaration = $0.baseAddress!
+            }
+            rawValue.NumEntries = UInt32(newValue.count)
+        }
+    }
+
+    /// An array of buffer strides; each stride is the size of an element for that buffer.
+    public var bufferStrides: [UInt32] {
+        get {
+            let buffer = UnsafeBufferPointer(start: rawValue.pBufferStrides, count: Int(rawValue.NumStrides))
+            return Array(buffer)
+        }
+        set {
+            newValue.withUnsafeBufferPointer {
+                rawValue.pBufferStrides = $0.baseAddress!
+            }
+            rawValue.NumStrides = UInt32(newValue.count)
+        }
+    }
+
+    /// The index number of the stream to be sent to the rasterizer stage.
+    public var rasterizedStream: UInt32 {
+        get {
+            return rawValue.RasterizedStream
+        }
+        set {
+            rawValue.RasterizedStream = newValue
+        }
+    }
+
+    /** Describes a streaming output buffer.
+    - parameter declarationEntries: An array of D3D12_SO_DECLARATION_ENTRY structures. Can't be NULL if NumEntries > 0.
+    - parameter bufferStrides: An array of buffer strides; each stride is the size of an element for that buffer.
+    - parameter rasterizedStream: The index number of the stream to be sent to the rasterizer stage.
+    */
+    public init(declarationEntries: [D3DStreamOutputDeclarationEntry], bufferStrides: [UInt32], rasterizedStream: UInt32) {
+        self.rawValue = RawValue()
+        self.declarationEntries = declarationEntries
+        self.bufferStrides = bufferStrides
+        self.rasterizedStream = rasterizedStream
+    }
 
     internal init(_ rawValue: RawValue) {
         self.rawValue = rawValue
