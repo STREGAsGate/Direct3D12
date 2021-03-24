ae67da4277175f19d62ca28e032ce6b0db98ca56
Dustin Collins <55062873+STREGAsGate@users.noreply.github.com>
2021-03-24 18:01:06 -0400

    implement D3DTexture2DRenderTargetView.

--- Sources/Direct3D12/d3d12_h/Structures/D3D12_TEX2D_RTV.swift
+++ Sources/Direct3D12/d3d12_h/Structures/D3D12_TEX2D_RTV.swift
@@ -8,10 +8,39 @@
 
 import WinSDK
 
+/// Describes the subresource from a 2D texture to use in a render-target view.
 public struct D3DTexture2DRenderTargetView {
     public typealias RawValue = WinSDK.D3D12_TEX2D_RTV
     internal var rawValue: RawValue
 
+    /// The index of the mipmap level to use.
+    public var mipIndex: UInt32 {
+        get {
+            return rawValue.MipSlice
+        }
+        set {
+            rawValue.MipSlice = newValue
+        }
+    }
+
+    /// The index (plane slice number) of the plane to use in the texture.
+    public var planeSlice: UInt32 {
+        get {
+            return rawValue.PlaneSlice
+        }
+        set {
+            rawValue.PlaneSlice = newValue
+        }
+    }
+
+
+    /** Describes the subresource from a 2D texture to use in a render-target view.
+    - parameter mipIndex: 
+    - parameter planeSlice: 
+    */
+    public init(mipIndex: UInt32, planeSlice: UInt32) {
+        self.rawValue = RawValue(MipSlice: mipIndex, PlaneSlice: planeSlice)
+    }
 
     internal init(_ rawValue: RawValue) {
         self.rawValue = rawValue
