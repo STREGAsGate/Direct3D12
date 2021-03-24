46323e98110c7e3e65e08df1416352f93f54db51
Dustin Collins <55062873+STREGAsGate@users.noreply.github.com>
2021-03-24 18:27:40 -0400

    Implement D3DTextureCubeArrayShaderResourceView.

--- Sources/Direct3D12/d3d12_h/Structures/D3D12_TEX3D_SRV.swift
+++ Sources/Direct3D12/d3d12_h/Structures/D3D12_TEX3D_SRV.swift
@@ -44,10 +44,9 @@ public struct D3DTexture3DShaderResourceView {
     }
 
     /** Describes the subresources from a 3D texture to use in a shader-resource view.
-    - parameter maxMipLevel: 
-    - parameter mipLevels: 
-    - parameter planeSlice: 
-    - parameter minLODClamp: 
+    - parameter maxMipLevel: Index of the most detailed mipmap level to use; this number is between 0 and MipLevels (from the original Texture3D for which ID3D12Device::CreateShaderResourceView creates a view) -1.
+    - parameter mipLevels: The maximum number of mipmap levels for the view of the texture. See the remarks in D3D12_TEX1D_SRV. Set to -1 to indicate all the mipmap levels from MostDetailedMip on down to least detailed.
+    - parameter minLODClamp: A value to clamp sample LOD values to. For example, if you specify 2.0f for the clamp value, you ensure that no individual sample accesses a mip level less than 2.0f.
     */
     public init(maxMipLevel: UInt32, mipLevels: UInt32, minLODClamp: Float) {
         self.rawValue = RawValue(MostDetailedMip: maxMipLevel,
