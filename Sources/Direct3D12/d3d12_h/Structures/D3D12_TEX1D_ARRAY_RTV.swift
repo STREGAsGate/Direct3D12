2b9c41ab6bae694199a3d3660d71e2bb4c2b6615
Dustin Collins <55062873+STREGAsGate@users.noreply.github.com>
2021-03-24 17:40:24 -0400

    Implement D3DTexture1DArrayRenderTargetView.

--- Sources/Direct3D12/d3d12_h/Structures/D3D12_TEX1D_ARRAY_RTV.swift
+++ Sources/Direct3D12/d3d12_h/Structures/D3D12_TEX1D_ARRAY_RTV.swift
@@ -12,6 +12,46 @@ public struct D3DTexture1DArrayRenderTargetView {
     public typealias RawValue = WinSDK.D3D12_TEX1D_ARRAY_RTV
     internal var rawValue: RawValue
 
+    /// The index of the first mipmap level to use.
+    public var mipIndex: UInt32 {
+        get {
+            return rawValue.MipSlice
+        }
+        set {
+            rawValue.MipSlice = newValue
+        }
+    }
+
+    /// The index of the first texture to use in an array of textures.
+    public var textureIndex: UInt32 {
+        get {
+            return rawValue.FirstArraySlice
+        }
+        set {
+            rawValue.FirstArraySlice = newValue
+        }
+    }
+
+    /// Number of textures to use.
+    public var textureCount: UInt32 {
+        get {
+            return rawValue.ArraySize
+        }
+        set {
+            rawValue.ArraySize = newValue
+        }
+    }
+
+    /** Describes the subresources from an array of 1D textures to use in a depth-stencil view.
+    - parameter mipIndex: The index of the first mipmap level to use.
+    - parameter textureIndex: The index of the first texture to use in an array of textures.
+    - parameter textureCount: Number of textures to use.
+    */
+    public init(mipIndex: UInt32, textureIndex: UInt32, textureCount: UInt32) {
+        self.rawValue = RawValue(MipSlice: mipIndex,
+                                 FirstArraySlice: textureIndex,
+                                 ArraySize: textureCount)
+    }
 
     internal init(_ rawValue: RawValue) {
         self.rawValue = rawValue
