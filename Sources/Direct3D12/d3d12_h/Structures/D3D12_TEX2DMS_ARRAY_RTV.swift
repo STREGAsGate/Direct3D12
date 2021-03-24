38986d5cc127ed84c476ca85f5e74f34ada0a209
Dustin Collins <55062873+STREGAsGate@users.noreply.github.com>
2021-03-24 18:15:27 -0400

    add D3DTexture2DMultiSampledArrayRenderTargetView

--- Sources/Direct3D12/d3d12_h/Structures/D3D12_TEX2DMS_ARRAY_RTV.swift
+++ Sources/Direct3D12/d3d12_h/Structures/D3D12_TEX2DMS_ARRAY_RTV.swift
@@ -8,10 +8,38 @@
 
 import WinSDK
 
+/// Describes the subresources from an array of multi sampled 2D textures to use in a render-target view.
 public struct D3DTexture2DMultiSampledArrayRenderTargetView {
     public typealias RawValue = WinSDK.D3D12_TEX2DMS_ARRAY_RTV
     internal var rawValue: RawValue
 
+    /// The index of the first texture to use in an array of textures.
+    public var textureIndex: UInt32 {
+        get {
+            return rawValue.FirstArraySlice
+        }
+        set {
+            rawValue.FirstArraySlice = newValue
+        }
+    }
+
+    /// Number of textures to use.
+    public var textureCount: UInt32 {
+        get {
+            return rawValue.ArraySize
+        }
+        set {
+            rawValue.ArraySize = newValue
+        }
+    }
+
+    /** Describes the subresources from an array of multi sampled 2D textures to use in a render-target view.
+    - parameter firstArraySlice: The index of the first texture to use in an array of textures.
+    - parameter arraySize: Number of textures to use.
+    */
+    public init(textureIndex: UInt32, textureCount: UInt32) {
+        self.rawValue = RawValue(FirstArraySlice: textureIndex, ArraySize: textureCount)
+    }
 
     internal init(_ rawValue: RawValue) {
         self.rawValue = rawValue
