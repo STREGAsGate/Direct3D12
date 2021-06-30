66030b677fca701ff1c9ba2cde7e12947e531a8f
Strega's Gate <55062873+STREGAsGate@users.noreply.github.com>
2021-06-30 15:47:09 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,26 +3,23 @@
  * Copyright (c) 2020 - 2021 Dustin Collins (Strega's Gate)
  * All Rights Reserved.
  * Licensed under Apache License v2.0
- * 
+ *
  * Find me on https://www.YouTube.com/STREGAsGate, or social media @STREGAsGate
  */
 
 import PackageDescription
 
-#if os(Windows)
 let package = Package(
     name: "Direct3D12",
     products: [
-        .library(
-            name: "Direct3D12", targets: ["Direct3D12"]),
+        .library(name: "Direct3D12", targets: ["Direct3D12"]),
     ],
     dependencies: [
-        // .package(name: "Direct3D12_Enumerations", path: "./Sources/Direct3D12_Enumerations"),
+        
     ],
     targets: [
         .target(
             name: "Direct3D12",
-            // dependencies: ["Direct3D12_Enumerations"],
             swiftSettings: [
                 .define("Direct3D12ExcludeOriginalStyleAPI", .when(configuration: .release)),
             ],
@@ -37,10 +34,3 @@ let package = Package(
         ),
     ]
 )
-#else
-#if !os(Windows)
-#warning("Direct3D12 is only available on the Windows platform.")
-#endif
-
-let package = Package(name: "Direct3D12")
-#endif
